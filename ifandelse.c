bd4274dbd1b6c14710374d5a42d1e913ca336028
ABHISHEK GADAD <abhishekgadad566@gmail.com>
2026-09-06 17:51:47 +0530

    Add files via upload

---
+++ ifandelse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5, b = 9;
+    if (a>b)
+    {
+        printf("a is greater than b");                                                                                                                                                                    
+    }
+    else
+    {
+        printf("a is less than b");
+    }
+    return 0;
+}
