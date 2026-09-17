97ab85f6a66d56dd0c403eb5ee7a28e77f3e9b72
ABHISHEK GADAD <abhishekgadad566@gmail.com>
2026-09-17 17:42:48 +0530

    Add files via upload

---
+++ callbyreference.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap( int *, int *);
+int main()
+{
+    int a= 10;
+    int b = 20;
+    printf("\n before swap value of a = %d and b = %d", a, b);
+    swap(&a, &b);
+    printf("\n after swap value of a = %d and b = %d", a, b);
+}
+
+void swap( int *a, int *b)
+
+{
+    int temp;
+    temp = *a;   
+    *a=*b;
+    *b=temp;    
+}
