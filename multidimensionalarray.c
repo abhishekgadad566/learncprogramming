bf3187fce0d2b4bd4f43fcc0df2ac5bc759218f5
ABHISHEK GADAD <abhishekgadad566@gmail.com>
2026-09-19 16:33:33 +0530

    Add files via upload

---
+++ multidimensionalarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int x[2][3][2]=
+    {
+
+        {{0,1},{2,3},{4,5}},
+        {{6,7},{8,9},{10,11}}
+    };
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            for(int k=0; k<2; k++)
+            {
+                printf("\nelement at x[%d][%d][%d] = %d", i, j, k, x[i][j][k]);
+
+            }
+        }
+    }
+    return 0;   
+}
