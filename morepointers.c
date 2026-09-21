f8dcd0a5d7b28634b89da27d824574209e862a54
ABHISHEK GADAD <abhishekgadad566@gmail.com>
2026-09-21 17:29:21 +0530

    Add files via upload

---
+++ morepointers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr=NULL;
+    printf("the value inside variable ptr is:%d \n",ptr);
+
+    void *p=NULL;
+    printf("The size of the pointer is:5d\n",sizeof(p));
+
+    int *pw;
+    printf("\n%d",pw);
+
+    return 0;
+}
